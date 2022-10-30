d51dde4fce97743172a24ef9071d54897a0e59dc
draguleee <aandreid14@gmail.com>
2022-10-30 19:26:03 +0200

    C# Testing

---
+++ testing-csharp/testing-csharp/Course1/Browser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_csharp.Course1
+{
+    // Class
+    public class Browser
+    {
+        // Variable
+        public int WindowWidth = 1024;
+
+        // Default constructor
+        public Browser() { }
+
+        // Constructor with parameters
+        public Browser(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+            Console.WriteLine("New window width: " + WindowWidth);
+        }
+
+        // Method
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+            Console.WriteLine("New window width: " + WindowWidth);
+        }
+    }
+}
