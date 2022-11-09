5121e847e7eeef72c3520eaf7126873dc7057cb6
draguleee <aandreid14@gmail.com>
2022-11-09 21:49:11 +0200

    C# Testing

--- testing-csharp/testing-nunit/Course6_Part2/Course6_Part2_HW/Browser.cs
+++ testing-csharp/testing-nunit/Course6_Part2/Course6_Part2_HW/Browser.cs
@@ -8,5 +8,26 @@ namespace testing_nunit.Course6_Part2.Course6_Part2_HW
 {
     public class Browser
     {
+        // Class variable
+        public int WindowWidth = 1024;
+
+        // Method to start the browser
+        public bool StartBrowser()
+        {
+            Console.WriteLine("Browser started.");
+            return true;
+        }
+
+        // Method to get the browser title
+        public string GetPageTitle()
+        {
+            return "Browser is open.";
+        }
+
+        // Method to get the browser's width
+        public int GetBrowserWidth()
+        {
+            return WindowWidth;
+        }
     }
 }
---
+++ testing-csharp/testing-nunit/Course6_Part2/Course6_Part2_HW/Test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_nunit.Course6_Part2.Course6_Part2_HW
+{
+    [TestFixture]
+    public class Test
+    {
+        // Create a Browser object
+        Browser browser = new Browser();
+
+        [Test]
+        public void TestStartBrowser()
+        {
+            Assert.IsTrue(browser.StartBrowser());      // Expected - true, Returned - true
+            Assert.IsFalse(browser.StartBrowser());     // Expected - false, Returned - true
+        }
+
+        [Test]
+        public void TestGetPageTitle()
+        {
+            Assert.AreEqual("Browser is open.", browser.GetPageTitle());        // Expected - equal, Returned - equal
+            Assert.AreEqual("No Browser open.", browser.GetPageTitle());        // Expected - equal, Returned - not equal
+            Assert.AreNotEqual("Browser is open.", browser.GetPageTitle());     // Expected - not equal, Returned - equal
+            Assert.AreNotEqual("No Browser open.", browser.GetPageTitle());     // Expected - not equal, Returned - not equal
+        }
+
+        [Test]
+        public void TestGetBrowserWidth()
+        {
+            Assert.AreEqual(1024, browser.GetBrowserWidth());       // Expected - equal, Returned - equal
+            Assert.AreEqual(1920, browser.GetBrowserWidth());       // Expected - equal, Returned - not equal
+            Assert.Less(1440, browser.GetBrowserWidth());           // Expected - less, Returned - less
+            Assert.Less(920, browser.GetBrowserWidth());            // Expected - less, Returned - greater
+            Assert.Greater(920, browser.GetBrowserWidth());         // Expected - greater, Returned - greater
+            Assert.Greater(2040, browser.GetBrowserWidth());        // Expected - greater, Returned - less
+        }
+    }
+}
